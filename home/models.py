c8906e747922d672a2d551a3240dcf22ec7cf792
Roosa <roosa.paivansalo@codeclan.com>
2021-10-05 22:48:17 +0100

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import related
+
+from wagtail.core.models import Page
+from wagtail.core.fields import RichTextField
+from wagtail.admin.edit_handlers import FieldPanel, PageChooserPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+class HomePage(Page):
+
+    templates = "templates/home/home_page.html"
+    max_count = 1
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+    banner_cta = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title"),
+        FieldPanel("banner_subtitle"),
+        ImageChooserPanel("banner_image"),
+        PageChooserPanel("banner_cta"),
+    ]
+
+    class Meta:
+
+        verbose_name = "KAAMOS STUDIO HOME PAGE"
+        verbose_name_plural = "KAAMOS STUDIO HOME PAGES"
+
+class ProductPage(Page):
+    pass
---
+++ kaamos_shop/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-u4m&p8thuq$bx6^4xn#v76vd+i8u6-g)i%@p-358+8vx%u$ejo'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
