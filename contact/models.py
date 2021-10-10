abede7603ad0e9f099363b5a222f8c7aa199433e
Roosa <roosa.paivansalo@codeclan.com>
2021-10-10 22:21:17 +0100

    Add contact page draft

---
+++ contact/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from wagtail.core.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.admin.edit_handlers import FieldPanel, FieldRowPanel, InlinePanel, MultiFieldPanel
+
+from modelcluster.fields import ParentalKey
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey(
+        'ContactPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields',
+    )
+
+class ContactPage(AbstractEmailForm):
+
+    template = "contact/contact_page.html"
+    landing_page_template = "contact/contact_page_landing.html"
+
+    intro = RichTextField(blank=False, null=True)
+    thank_you_text = RichTextField(blank=False, null=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro', classname="full"),
+        InlinePanel('form_fields', label='Form Fields'),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel("subject"),
+        ], heading="Email Settings"),
+    ]
--- kaamos_shop/settings/base.py
+++ kaamos_shop/settings/base.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'search',
     'product',
     'about',
+    'contact',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
